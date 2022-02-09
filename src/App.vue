84a7393bda8b0a2cfd6d547422d793669959cda2
chenjiale <2602984080@qq.com>
2022-02-09 20:38:43 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+ <div>
+  御剑乘风来,除魔天地间!
+ </div>
 </template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
     }
+  },
+  created () {
+  },
+  computed: {
+  },
+  methods: {
   }
 }
+</script>
+<style lang='less'  scoped>
+
 </style>
